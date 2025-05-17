2dad0eca6b2849b1770d197716319bb1212c1d21
Fasel Godbcho <faselgodbcho@gmail.com>
2025-05-17 00:53:45 -0300

    Remove unnecessary line

--- src/components/Home/ChatBody.tsx
+++ src/components/Home/ChatBody.tsx
@@ -1,53 +1,139 @@
-import { useState } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
+import { useNavigate } from "react-router-dom";
+// import { useAuth } from "../../contexts/AuthContext";
 
 export default function ChatBody() {
-  const [chats, setChat] = useState([
-    {
-      id: 1,
-      name: "User 1",
-      message: "message by user 0 and it hjfhj jgh hkhv is shown sth",
-      me: false,
-    },
-    {
-      id: 2,
-      name: "User 1",
-      message: "message by user 0 and it hjfhj jgh hkhv is shown sth",
-      me: true,
-    },
-    {
-      id: 3,
-      name: "User 1",
-      message: "message by user 0 and it hjfhj jgh hkhv is shown sth",
-      me: false,
-    },
-    {
-      id: 4,
-      name: "User 1",
-      message: "message by user 0 and it hjfhj jgh hkhv is shown sth",
-      me: true,
-    },
-    {
-      id: 5,
-      name: "User 1",
-      message: "message by user 0 and it hjfhj jgh hkhv is shown sth",
-      me: false,
-    },
-    {
-      id: 6,
-      name: "me",
-      message: "message by user 0 and it hjfhj jgh hkhv is shown sth",
-      me: true,
-    },
-  ]);
+  // const { credentials } = useAuth();
+  const navigate = useNavigate();
+
+  const credentials = JSON.parse(localStorage.getItem("authData") || "{}");
+
+  useLayoutEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const accesstoken = credentials.access;
+        const res = await fetch(
+          "https://techclub-6c39263b7807.herokuapp.com/chatlist/",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: `Bearer ${accesstoken}`,
+            },
+            credentials: "include",
+          }
+        );
+
+        const data = await res.json();
+
+        if (res.status === 401) {
+          console.log("Unauthorized access. Please login again.");
+          navigate("/auth");
+        }
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch chats");
+        }
+
+        console.log("data", data);
+        setChat(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchChats();
+
+    const validAccessToken = async () => {
+      try {
+        if (!credentials || !credentials.access) {
+          console.log("No AccessToken detected. Please login");
+          navigate("/auth");
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    validAccessToken();
+  }, [credentials, navigate]);
+
+  // const [chats, setChat] = useState<
+  //  { id: number; name: string; message: string; me: boolean }[] | []
+  // >([]);
+  const [message, setMessage] = useState<string>("");
+  useEffect(() => {
+    const fetchChats = async () => {
+      try {
+        const accesstoken = credentials.access;
+        const res = await fetch(
+          "https://techclub-6c39263b7807.herokuapp.com/chatlist/",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: `Bearer ${accesstoken}`,
+            },
+            credentials: "include",
+          }
+        );
+
+        const data = await res.json();
+
+        if (res.status === 401) {
+          console.log("Unauthorized access. Please login again.");
+          navigate("/auth");
+        }
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch chats");
+        }
+
+        console.log("data", data);
+        setChat(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchChats();
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(
+        "https://techclub-6c39263b7807.herokuapp.com/chat/",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${credentials.access}`,
+          },
+          body: JSON.stringify({
+            message,
+          }),
+        }
+      );
+
+      const data = await res.json();
+      console.log(data);
+      setMessage("");
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div className="flex flex-col mt-16 rounded-2xl bg-[#535151] w-[800px] h-[500px]">
       <div className="flex flex-col w-full h-full gap-8 p-8 overflow-auto scrollbar-hide max-w-[800px]">
-        {/* {chats.map((chat) => (
+        {chats.map((chat) => (
           <div
             className={`flex gap-2 items-center ${
               chat.me ? "self-end" : "self-start"
             }`}
+            key={chat.id}
           >
             {!chat.me && (
               <div className="w-20 h-20 rounded-full bg-white mx-auto mt-4"></div>
@@ -60,14 +146,16 @@ export default function ChatBody() {
               <p className="text-[14px]"> {chat.message}</p>
             </div>
           </div>
-        ))} */}
+        ))}
       </div>
       <div className="px-8 py-4 bg-[#383838]">
-        <form className="flex items-center gap-4">
+        <form className="flex items-center gap-4" onSubmit={handleSubmit}>
           <input
             type="text"
             placeholder="Write a message..."
             className="px-4 py-4 w-[356px] rounded-2xl bg-[#D9D9D9] outline-none"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />
 
           <span className="mt-1 cursor-pointer">
@@ -78,9 +166,9 @@ export default function ChatBody() {
               viewBox="0 0 24 24"
               fill="none"
               stroke="#fff"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
               <path d="M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z" />
               <path d="m21.854 2.147-10.94 10.939" />
--- src/components/Home/Sidebar.tsx
+++ src/components/Home/Sidebar.tsx
@@ -1,20 +1,95 @@
+import { useEffect, useState } from "react";
+
 export default function Sidebar() {
-  const user = {
-    name: "Henok Henon",
-    id: 9878,
-    grade: 10,
-    subClass: "Developer",
+  const [user, setUser] = useState<
+    { name: string; id: string; grade: number; subClass: string } | undefined
+  >(undefined);
+
+
+  const credentials = JSON.parse(localStorage.getItem("authData") || "{}");
+  const [profileImg, setProfileImg] = useState<string>("")
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch("https://techclub-6c39263b7807.herokuapp.com", {
+          method: "GET",
+          headers: {
+            authorization: `Bearer ${credentials.access}`,
+          },
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        setUser({
+          name: data.name,
+          grade: data.grade,
+          id: data.unique_id,
+          subClass: data.sub_class,
+        });
+
+        setProfileImg(data.profile)
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchUser();
+  }, []);
+
+  const handleProfileChange = async (e: any) => {
+    const value =
+        URL.createObjectURL(e.target.files[0])
+        
+    setProfileImg(value)
+
+    const res = await fetch(
+      "https://techclub-6c39263b7807.herokuapp.com/profile/",
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: `Bearer ${credentials.access}`,
+        },
+        body: JSON.stringify({ pic: profileImg, bio: "" }),
+        credentials: "include",
+      }
+    );
   };
+
+
 
   return (
     <div className="flex flex-col gap-6 bg-[#383838] p-4 rounded-2xl h-full">
-      <div className="w-28 h-28 rounded-full bg-white mx-auto mt-4"></div>
+      <div className="w-32 h-32 mx-auto mt-4 relative">
+            <img
+                src={profileImg}
+                className="w-full h-full rounded-full"
+              />
+          <div>
+            <label
+              htmlFor="studentImage"
+              className="p-2 absolute bg-[#FF7F50] w-8 h-8 rounded-full -right-3 bottom-4 text-white bg-secondary cursor-pointer"
+            >
+              <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1.1em" width="1.1em" xmlns="http://www.w3.org/2000/svg"><path d="M17.263 2.177a1.75 1.75 0 0 1 2.474 0l2.586 2.586a1.75 1.75 0 0 1 0 2.474L19.53 10.03l-.012.013L8.69 20.378a1.753 1.753 0 0 1-.699.409l-5.523 1.68a.748.748 0 0 1-.747-.188.748.748 0 0 1-.188-.747l1.673-5.5a1.75 1.75 0 0 1 .466-.756L14.476 4.963ZM4.708 16.361a.26.26 0 0 0-.067.108l-1.264 4.154 4.177-1.271a.253.253 0 0 0 .1-.059l10.273-9.806-2.94-2.939-10.279 9.813ZM19 8.44l2.263-2.262a.25.25 0 0 0 0-.354l-2.586-2.586a.25.25 0 0 0-.354 0L16.061 5.5Z"></path></svg>
+            </label>
+          </div>
+
+          <input
+            className="hidden w-full py-2 rounded focus:outline-none px-7"
+            type="file"
+            id="studentImage"
+            name="studentImage"
+            accept="image/jpeg, image/png, image/jpg"
+            onChange={handleProfileChange}
+          />
+      </div>
 
       <div className="w-[250px] text-white">
-        <p> Name: {user.name}</p>
-        <p> Id: {user.id}</p>
-        <p> Grade: {user.grade}</p>
-        <p> Sub Class: {user.subClass}</p>
+        <p> Name: {user?.name}</p>
+        <p> Id: {user?.id}</p>
+        <p> Grade: {user?.grade}</p>
+        <p> Sub Class: {user?.subClass}</p>
       </div>
     </div>
   );
